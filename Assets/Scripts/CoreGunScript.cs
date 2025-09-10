198d438e299e36319b9867e0069206c7aa5e0e46
Dragonlord8781 <evan.stone638@gmail.com>
2025-09-10 09:50:30 -0500

    9/10/2025
    added functioning gun

--- Assets/Scripts/CoreGunScript.cs
+++ Assets/Scripts/CoreGunScript.cs
@@ -3,22 +3,45 @@ using UnityEngine;
 
 public class CoreGunScript : MonoBehaviour
 {
-    public UnityEvent onGunShoot;
-    public float fireCooldown;
+    public UnityEvent OnGunShoot;
+    public float FireCoolDown;
 
-    public bool automatic;
+    public bool Automatic;
 
-    private float currentCooldown;
+    private float CurrentCooldown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentCooldown = fireCooldown;
+        CurrentCooldown = FireCoolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Automatic)
+        {
+            if(Input.GetMouseButtonDown(0)) 
+            {
+                if(CurrentCooldown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCooldown = FireCoolDown;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCooldown = FireCoolDown;
+                }
+            }
+        }
+
+        CurrentCooldown -= Time.deltaTime;
     }
 }
---
+++ Assets/Scripts/EntityHealthScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealthScript : MonoBehaviour
+{
+    [SerializeField] private float StartingHealth;
+    private float health;
+
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            Debug.Log(health);
+
+            if (health <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Health = StartingHealth;
+    }
+}
