7368524ada6a1b7ce9b08af2f4d657512c3f258b
Dragonlord8781 <evan.stone638@gmail.com>
2026-02-18 10:45:33 -0600

    0.4.5.6

--- Assets/Scripts/FlameThrowerRadius.cs
+++ Assets/Scripts/FlameThrowerRadius.cs
@@ -1,16 +1,51 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FlameThrowerRadius : MonoBehaviour
 {
+    public bool burningEnemy;
+    private float fireDamage;
+    public GunDamageScript Source;
+
+    private List<GameObject> EnemiesInRadius = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        fireDamage = Source.Damage;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (GameObject obj in EnemiesInRadius)
+        {
+            if (obj != null && obj.TryGetComponent(out EntityHealthScript enemyHealth))
+            {
+                enemyHealth.Health -= fireDamage;
+                if (enemyHealth.Health < 0)
+                {
+                    EnemiesInRadius.Remove(obj);
+                }
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            burningEnemy = true;
+            EnemiesInRadius.Add(collision.gameObject);
+            Debug.Log("burning enemies");
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EnemiesInRadius.Remove(other.gameObject);
+
     }
 }
